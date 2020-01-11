ad67cf5c3c9d9ec381cfcfc910d8b38e9c2c6daf
SapuSeven <sapuseven@gmail.com>
2020-01-11 16:39:06 +0100

    Fixed power menu shutdown

--- .config/scripts/power-menu.sh
+++ .config/scripts/power-menu.sh
@@ -2,6 +2,6 @@
 
 rofi_command="rofi -width 200 -lines 5 -hide-scrollbar -fixed-num-lines"
 
-options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend'
+options=$'shutdown\nreboot\nlogout\nhibernate\nsuspend'
 
 eval i3exit $(echo "$options" | $rofi_command -dmenu -p "")
