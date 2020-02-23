6a0a0aadab3b26f8bb44b94ff3fd5d8438e1e76f
SapuSeven <sapuseven@gmail.com>
2020-02-23 23:55:06 +0100

    Changed lock screen

--- .config/scripts/lock.sh
+++ .config/scripts/lock.sh
@@ -2,4 +2,5 @@
 
 export DISPLAY=:0
 
-i3lock --blur=10 --clock --indicator --insidecolor=1F1F1FFF --insidevercolor=1F1F1FFF --insidewrongcolor=1F1F1FFF --ringcolor=1F1F1FFF --ringvercolor=43A047FF --ringwrongcolor=E53935FF --line-uses-inside --keyhlcolor=43A047FF --bshlcolor=43A047FF --separatorcolor=1F1F1FFF --verifcolor=FFFFFFFF --wrongcolor=FFFFFFFF --timecolor=FFFFFFFF --datecolor=FFFFFFFF
+multilockscreen -l
+#i3lock --blur=10 --clock --indicator --insidecolor=1F1F1FFF --insidevercolor=1F1F1FFF --insidewrongcolor=1F1F1FFF --ringcolor=1F1F1FFF --ringvercolor=43A047FF --ringwrongcolor=E53935FF --line-uses-inside --keyhlcolor=43A047FF --bshlcolor=43A047FF --separatorcolor=1F1F1FFF --verifcolor=FFFFFFFF --wrongcolor=FFFFFFFF --timecolor=FFFFFFFF --datecolor=FFFFFFFF
