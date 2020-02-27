0533cb170c1e3211faed9f3261fe930659f7dedd
SapuSeven <sapuseven@gmail.com>
2020-02-27 09:09:06 +0100

    Rearrange power menu entries

--- .config/scripts/power-menu.sh
+++ .config/scripts/power-menu.sh
@@ -2,6 +2,6 @@
 
 rofi_command="rofi -width 200 -lines 5 -hide-scrollbar -fixed-num-lines"
 
-options=$'logout\nhibernate\nsuspend\nreboot\nshutdown'
+options=$'suspend\nhibernate\nlogout\nreboot\nshutdown'
 
 eval i3exit $(echo "$options" | $rofi_command -dmenu -p "")
