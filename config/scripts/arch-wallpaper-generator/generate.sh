c21884618993c730ee9c488a198ea75378e90260
SapuSeven <sapuseven@gmail.com>
2021-03-04 01:06:50 +0100

    Update wallpaper generation script

--- config/scripts/arch-wallpaper-generator/generate.sh
+++ config/scripts/arch-wallpaper-generator/generate.sh
@@ -8,11 +8,11 @@ logoSize=300
 mkdir .tmp
 
 # Foreground
-magick convert MSVG:logo.svg .tmp/logo.mvg
-magick convert -background none MVG:.tmp/logo.mvg -fill $fgColor -colorize 100 -scale $logoSize .tmp/logo.png
+magick convert -background none logo.svg -fill $fgColor -colorize 100 -scale $logoSize .tmp/logo.png
 
 
 # Combine
-magick convert -size 1920x1080 xc:$bgColor .tmp/logo.png -gravity center -composite bg.png
+magick convert -size 1920x1080 xc:$bgColor .tmp/logo.png -depth 8 -gravity center -composite bg.png
+
 
 rm -r .tmp
