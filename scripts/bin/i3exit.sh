49e9ae0198de858946f21ae52a857fe621dd0fb0
SapuSeven <sapuseven@gmail.com>
2021-06-10 20:33:43 +0200

    Update i3exit script to work with the updated i3lock-color

--- scripts/bin/i3exit.sh
+++ scripts/bin/i3exit.sh
@@ -17,17 +17,18 @@ lock() {
 	 --color=$COLOR_BG \
 	 --force-clock \
 	 --pass-media-keys --pass-screen-keys --pass-power-keys --pass-volume-keys \
-	 --radius=250 --no-modkeytext\
-	 --indpos="x+w/2:y+h/2+70" \
-	 --timecolor=$COLOR_TEXT --timesize=50 --timepos="ix:iy+140" \
-	 --datestr="%A, %d.%m.%Y" --datecolor=$COLOR_TEXT --datesize=20 --datepos="tx:ty+40" \
-	 --verifsize=80 --wrongsize=80 \
-	 --bar-position="h" --bar-direction=1 \
-	 --insidecolor=$COLOR_NONE --insidevercolor=$COLOR_BG --insidewrongcolor=$COLOR_BG \
-	 --linecolor=$COLOR_BG --ring-width=10 --refresh-rate=3.0 \
-	 --ringcolor=$COLOR_NONE --ringvercolor=$COLOR_NONE --ringwrongcolor=$COLOR_NONE \
-	 --keyhlcolor=$COLOR_FG --bshlcolor=$COLOR_RED --separatorcolor=$COLOR_NONE \
-	 --verifcolor=$COLOR_FG --wrongcolor=$COLOR_RED
+	 --radius=250 \
+	 --no-modkey-text \
+	 --ind-pos="x+w/2:y+h/2+70" \
+	 --time-color=$COLOR_TEXT --time-size=50 --time-pos="ix:iy+140" \
+	 --date-str="%A, %d.%m.%Y" --date-color=$COLOR_TEXT --date-size=20 --date-pos="tx:ty+40" \
+	 --verif-size=80 --wrong-size=80 \
+	 --bar-pos="h" --bar-direction=1 \
+	 --inside-color=$COLOR_NONE --insidever-color=$COLOR_BG --insidewrong-color=$COLOR_BG \
+	 --line-color=$COLOR_BG --ring-width=10 --refresh-rate=3.0 \
+	 --ring-color=$COLOR_NONE --ringver-color=$COLOR_NONE --ringwrong-color=$COLOR_NONE \
+	 --keyhl-color=$COLOR_FG --bshl-color=$COLOR_RED --separator-color=$COLOR_NONE \
+	 --verif-color=$COLOR_FG --wrong-color=$COLOR_RED
 }
 
 case "$1" in
