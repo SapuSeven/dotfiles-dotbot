985990e80f3bda53bcad1b2d14d8df1344c5cc82
SapuSeven <sapuseven@gmail.com>
2022-02-23 10:08:00 +0100

    Update wallpaper generator

--- scripts/arch-wallpaper-generator/generate.sh
+++ scripts/arch-wallpaper-generator/generate.sh
@@ -2,17 +2,21 @@
 
 fgColor=#ffb52a
 bgColor=#222222
+logoName=arch
 logoSize=300
 
+# Customize the settings above in generate.conf
+
+test -f ./generate.conf && source ./generate.conf
 
 mkdir .tmp
 
 # Foreground
-magick convert -background none logo.svg -fill $fgColor -colorize 100 -scale $logoSize .tmp/logo.png
+magick convert -background none -density 500 logo-$logoName.svg -fill $fgColor -colorize 100 -scale $logoSize .tmp/logo-$logoName.png
 
 
 # Combine
-magick convert -size 1920x1080 xc:$bgColor .tmp/logo.png -depth 8 -gravity center -composite bg.png
+magick convert -size 1920x1080 xc:$bgColor .tmp/logo-$logoName.png -depth 8 -gravity center -composite bg.png
 
 
 rm -r .tmp
